6a682ece714f2aff1a6475b52ae601fd40715224
James White <jwhite@eliteworx.com>
2015-07-09 16:46:28 -0400

    Format changes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   namespace :api, defaults: { format: :json } do
                                 
-      scope module: :v1 do
-        resources :users, :only => [:show, :create, :update, :destroy]
-        resources :sessions, :only => [:create, :destroy]
-      end
+    scope module: :v1 do
+      resources :users, :only => [:show, :create, :update, :destroy]
+      resources :sessions, :only => [:create, :destroy]
     end
+  end
 end
