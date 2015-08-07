26bfc70208613b02adb9ba1a33871db62a681b7c
James White <jwhite@eliteworx.com>
2015-08-07 15:38:30 -0400

    fix restaurant index without location info

--- app/serializers/restaurant_serializer.rb
+++ app/serializers/restaurant_serializer.rb
@@ -1,4 +1,12 @@
 class RestaurantSerializer < ActiveModel::Serializer
   attributes :id, :name, :address, :city, :state, :postal_code, :logo, :premium_level, :phone_number, :website, :distance
+  
+  def distance
+    if object.respond_to? :distance
+      return object.distance
+    else
+      return ""
+    end
+  end
  
 end
