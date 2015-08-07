8f0189a92228cbdbaded709812a3f371c222aa3e
James White <jwhite@eliteworx.com>
2015-08-07 11:01:49 -0400

    adds description to dish detail

--- app/serializers/dish_serializer.rb
+++ app/serializers/dish_serializer.rb
@@ -1,5 +1,5 @@
 class DishSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :rating,  :updated_at, :user_rating, :restaurant
+  attributes :id, :name, :price, :rating, :description,  :updated_at, :user_rating, :restaurant
   has_many :dishpics, serializer: DishpicPreviewSerializer
   def user_rating
     user_rating = DishRating.where(user_id: @current_user).first
