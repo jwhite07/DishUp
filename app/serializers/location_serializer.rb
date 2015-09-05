43f45e29b3f56477189d67b9f005060f847a2b3c
James White <jwhite@eliteworx.com>
2015-09-05 15:41:25 -0400

    disable caching of distance

--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,7 +1,16 @@
 class LocationSerializer < ActiveModel::Serializer
-  cache key: "location", expires_in: 1.hours
-  attributes :id, :name, :address, :city, :state, :postal_code, :logo, :phone_number, :website, :distance, :restaurant
+  cache key: "location", expires_in: 1.hours, except: [:distance, :menu_id]
+  attributes :id, :name, :address, :city, :state, :postal_code, :logo, :phone_number, :website, :distance, :restaurant, :menu_id
+   
   
+  
+  def menu_id
+    if @special_event_id
+      SpecialEvent.find(@special_event_id).menus.where(restaurant_id: object.restaurant.id)
+    else
+      object.restaurant.default_menu.id
+    end
+  end
   def distance
     if object.respond_to? :distance
       return object.distance
