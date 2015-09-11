6b99995b14019381c6899ed8856934bbaaa45af3
James White <jwhite@eliteworx.com>
2015-09-11 13:15:58 -0400

    fixes html display on website

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  cache key: "user", expires_in: 1.hours
+  #cache key: "user", expires_in: 1.hours
   attributes :id, :name, :auth_token, :email
   
 end
